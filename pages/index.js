dbbd2dc02f47701a6306453eeecf9743cb4f2ec4
shimotsu4431 <shimotsu13@gmail.com>
2021-02-11 12:03:14 +0900

    add people data

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,19 @@
 import useSWR from 'swr'
-
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Person from '../components/Person'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
-export default function Home() {
-  const { data, error } = useSWR('/api/hello', fetcher)
-  console.log(data)
+export default function Index() {
+  const { data, error } = useSWR('/api/people', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Study Next API
-        </h1>
-
-        <p>{data.name}</p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <ul>
+      {data.map((p, i) => (
+        <Person key={i} person={p} />
+      ))}
+    </ul>
   )
 }
-
